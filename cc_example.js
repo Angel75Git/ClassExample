1f5fc4e49079fd6e2845f6f3505c2e1a98e1a377
Angel75Git <aduboncenteno@usf.edu>
2025-02-11 16:32:41 -0500

    Task 3 Complete

--- cc_example.js
+++ cc_example.js
@@ -13,19 +13,26 @@ let products = [
 
 //Task 2 
 
-function applyDiscount(products, discountRate){
-    return products.map(product =>({
-        ...product,
-    price: product.price- (product.price * discountRate),
-    name: product.name.toUpperCase()
-    }
+// function applyDiscount(products, discountRate){
+//     return products.map(product =>({
+//         ...product,
+//     price: product.price- (product.price * discountRate),
+//     name: product.name.toUpperCase()
+//     }
         
-    ))
-}
+//     ))
+// }
 
-console.log(applyDiscount(products, 0.1));
-console.log("*".repeat(20));
-console.log(products);
+// console.log(applyDiscount(products, 0.1));
+// console.log("*".repeat(20));
+// console.log(products);
 
 
 //Task 3
+
+let sales = [250, 400, 150, 900, 1200];
+
+function caluculateTotalRevenue(sales){
+    return sales.reduce((total, sale) => total + sale, 0)
+}
+console.log(`Total Rev. 2024: $${caluculateTotalRevenue(sales)}`);
