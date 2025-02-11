f299e4600404132ed4e8932cd26173b79712338c
Angel75Git <aduboncenteno@usf.edu>
2025-02-11 16:27:29 -0500

    Completed Task 2

--- cc_example.js
+++ cc_example.js
@@ -6,9 +6,26 @@ let products = [
     {name: "HeadPhones", price: 100, category: "Electronics"}
 ]
 
-// function getProductsByCategory(products, category){
-//     return products.filter(products => products.category === category);
+// function getProductsByCategory(product_array, category){
+//     return product_array.filter(product => product.category === category);
 // }
 // console.log(getProductsByCategory(products, "Electoronics"))
 
-//Task 2
+//Task 2 
+
+function applyDiscount(products, discountRate){
+    return products.map(product =>({
+        ...product,
+    price: product.price- (product.price * discountRate),
+    name: product.name.toUpperCase()
+    }
+        
+    ))
+}
+
+console.log(applyDiscount(products, 0.1));
+console.log("*".repeat(20));
+console.log(products);
+
+
+//Task 3
