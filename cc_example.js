089d42bbb27912c59cd81e717ed8d8ee126730dd
Angel75Git <aduboncenteno@usf.edu>
2025-02-11 16:06:23 -0500

    Completed Task1

--- cc_example.js
+++ cc_example.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+//Task 1
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800,category: "Electronics"},
+    {name: "Desk Chair", price: 150, category: "Furniture"},
+    {name: "HeadPhones", price: 100, category: "Electronics"}
+]
+
+// function getProductsByCategory(products, category){
+//     return products.filter(products => products.category === category);
+// }
+// console.log(getProductsByCategory(products, "Electoronics"))
+
+//Task 2
